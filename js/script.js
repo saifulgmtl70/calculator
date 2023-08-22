71d282450fdb41c2fa31058caedddfc9f8baf6f7
Saiful Islam <98213668+azadgmtl@users.noreply.github.com>
2023-08-22 21:14:14 +0600

    Slightly Changed

--- js/script.js
+++ js/script.js
@@ -1,33 +1,35 @@
-var input = "";
+var inputValue = "";
 
-function calcFunc(value){
-    input += value;
-    document.getElementById("displayField").value =  input;
+function calculator(value){
+    inputValue += value;
+    const display = document.getElementById("displayField");
+    display.value = inputValue;
+
 }
 
 
-function calculate(){
+function calCulate(){
 
-        const result = eval(input);
-        document.getElementById("displayField").value = result;
-        input = result.toString();
-
-        if(input === "=" && input !== ""){
-            input = eval(input.replace("%", "/100"));
+        try {
+            inputValue = inputValue.replace('%', '*0.01');
+            const calc = eval(inputValue);
+            const showResult = document.getElementById("displayField");
+            showResult.value = calc;
+            
+        } 
+        catch (error) {
+            alert("Invalid Calculation");
         }
-  
-    
+}
+
+function deleteBtn(){
+    inputValue = inputValue.slice(0, -1);
+    const displayDel = document.getElementById("displayField");
+    displayDel.value = inputValue;
 }
 
 
-function deleteFunc(){
-
-        input = input.slice(0, -1);
-        document.getElementById("displayField").value = input
-    
-}
-
-function clearFunc(){
-    input = "";
-    document.getElementById("displayField").value = input;
+function clearValue(){
+    inputValue = '';
+    document.getElementById("displayField").value = inputValue;
 }
